9f11d4e80bcd02a405ed181b026194f081497326
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-10 21:21:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,13 @@
+// Q3
+// console.log("'Albert Einstein once said,'A person who never made a mistake never tried anything new!'")
+// Q4
+// let famous_person: string = "Albert Einstein";
+// let message = "'A person who never made a mistake never tried anything new!'"
+// console.log(famous_person,message)
+// Q5
+// Whitespace
+// let personName: string = "\t\n KIRAN\t\n"; 
+// name with whitespace
+// console.log(personName);
+// name without whitespace
+// console.log(personName.trim())
